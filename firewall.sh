67a27603cfdb1d454271ed93bd74ea48121b864e
root <root@jessie.raw>
2018-03-25 21:43:44 +0000

    projeto_grupo2

---
+++ firewall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+iptables -F
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --sport 53 -j ACCEPT 
